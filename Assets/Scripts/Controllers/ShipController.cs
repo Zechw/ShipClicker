b889e4e51ba0c00ea37292383b63916fd1f37380
Zechw <zechwilliams@gmail.com>
2017-07-30 19:27:22 -0600

    Add files via upload

---
+++ Assets/Scripts/Controllers/ShipController.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShipController : MonoBehaviour {
+
+    //todo base ship, extended into player ship & enemy ship(s)
+
+    public float power = 30;
+    public float powerDrainRate = 5;
+    public float powerGainPerClick = 5;
+    public float laserPowerConsumption = 2;
+    public float damageTakenOnHit = 10;
+
+    public bool unlimitedPower = false;
+    public bool noDeath = false;
+
+    private float minX = -22;
+    private float maxX = -10;
+    private float minY = -11;
+    private float maxY = 11;
+    private float moveSpeed = 10;
+
+    public Transform target;
+    public Transform laser;
+    public ParticleSystem explosion;
+    public ParticleSystem shield;
+    public ParticleSystem shieldsDown;
+    private ParticleSystem shieldWarning1;
+    private ParticleSystem shieldWarning2;
+
+
+    [HideInInspector] public LevelManager activeLevel;
+
+    private bool mouseIsOver;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //keyboard controls!
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(x, y) * moveSpeed * Time.deltaTime, Space.World);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), Mathf.Clamp(transform.position.y, minY, maxY));
+
+
+
+
+        power -= powerDrainRate * Time.deltaTime;
+        transform.Find("alpha_mask").GetComponent<SpriteMask>().alphaCutoff = power / 100;
+
+        if(power <= 10 && shieldWarning1 == null && shieldWarning2 == null)
+        {
+            shieldWarning1 = Instantiate(shieldsDown, transform.position, Quaternion.identity, transform);
+            shieldWarning2 = Instantiate(shieldsDown, transform.position, Quaternion.Euler(0, 0, 180), transform);
+        }
+
+        if (power > 10 && shieldWarning1 != null && shieldWarning2 != null)
+        {
+            Destroy(shieldWarning1);
+            Destroy(shieldWarning2);
+        }
+        
+
+        //track to target
+        Vector3 diff = target.position - transform.position;
+        float angle = (Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg) - 90;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+
+        //fire weapon
+        if(Input.GetMouseButtonDown(0) && !mouseIsOver)
+        {
+            FireWeapon();
+        }
+
+        power = Mathf.Clamp(power, 0, 100);
+    }
+
+    void OnMouseDown()
+    {
+        power += powerGainPerClick;
+        activeLevel.AddScore(1);
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseIsOver = true;
+        target.GetComponent<SpriteRenderer>().color = Color.green;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseIsOver = false;
+        target.GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        power -= damageTakenOnHit;
+        if (power <= 0 && !noDeath)
+        {
+            Die();
+        }
+        else
+        {
+            ParticleSystem shield1 = Instantiate(shield, transform.position, collision.transform.rotation);
+            ParticleSystem shield2 = Instantiate(shield, transform.position, collision.transform.rotation);
+            shield1.transform.Rotate(0, 0, -90 -5);
+            shield2.transform.Rotate(0, 180, -90 -5);
+            Destroy(shield1.gameObject, 2);
+            Destroy(shield2.gameObject, 2);
+        }
+        Destroy(collision.gameObject);
+    }
+
+    private void Die()
+    {
+        AudioSource s = Instantiate(activeLevel.boomSound);
+        Destroy(s, 3);
+
+        ParticleSystem expClone1 = Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(expClone1.gameObject, 3);
+
+        ParticleSystem expClone2 = Instantiate(explosion, transform.position, Quaternion.identity);
+        expClone2.transform.localScale.Scale(Vector3.one * 4);
+        Destroy(expClone2.gameObject, 3);
+
+        Destroy(shieldWarning1);
+        Destroy(shieldWarning2);
+
+        Destroy(gameObject);
+    }
+
+    void FireWeapon()
+    {
+        if (power < laserPowerConsumption && !unlimitedPower) return;
+
+        AudioSource s = Instantiate(activeLevel.laserSound);
+        Destroy(s.gameObject, 1);
+
+        power -= laserPowerConsumption;
+        Transform laserClone1 = Instantiate(laser, transform.position, transform.rotation);
+        laserClone1.GetComponent<LaserController>().offset(0.2f);
+        Physics2D.IgnoreCollision(laserClone1.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+
+        Transform laserClone2 = Instantiate(laser, transform.position, transform.rotation);
+        laserClone2.GetComponent<LaserController>().offset(-0.2f);
+        Physics2D.IgnoreCollision(laserClone2.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public int score;
+    private int highScore;
+
+    public Canvas levelUI;
+    [HideInInspector] public Canvas activeUI;
+
+    public Transform starField;
+    private Transform activeStarField;
+
+    public Transform playerShip;
+    [HideInInspector] public Transform activePlayer;
+
+    public Transform asteroid;
+
+    public Transform enemy;
+    private bool enemiesAreSpawning = false;
+
+    public Transform boss;
+    private Transform activeBoss;
+
+    public AudioSource laserSound;
+    public AudioSource boomSound;
+
+    private bool isRunning = true;
+
+	void Start () {
+        activeUI = Instantiate(levelUI);
+        activeUI.transform.Find("Play Again").GetComponent<Button>().onClick.AddListener(startGame);
+
+        activeStarField = Instantiate(starField);
+        activeStarField.GetComponent<StarFieldController>().activeLevel = this;
+        
+        InvokeRepeating("SpawnAsteroid", 0, 2);
+
+        startGame();
+    }
+	
+	void Update () {
+		if (activePlayer == null && isRunning)
+        {
+            endGame(false);
+        }
+
+        if (score > 50 && score < 300)
+        {
+            activeUI.transform.Find("Tutorial").GetComponent<Text>().text = "Your shields will absorb lasers, as long as you have enough power.";
+        }
+        else if (score > 300)
+        {
+            activeUI.transform.Find("Tutorial").GetComponent<Text>().text = "Large Enemy Incoming!!";
+        }
+
+        if (score > 100 && enemiesAreSpawning == false && isRunning)
+        {
+            InvokeRepeating("SpawnEnemy", 2, 4);
+            enemiesAreSpawning = true;
+        }
+
+        if (score > 300 && activeBoss == null && isRunning)
+        {
+            activeBoss = Instantiate(boss, new Vector3(30, 20, 0), Quaternion.Euler(0, 0, -90));
+            activeBoss.GetComponent<BossController>().activeLevel = this;
+        }
+	}
+
+    public void startGame()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            Destroy(enemies[i]);
+        }
+
+        activeUI.transform.Find("Tutorial").GetComponent<Text>().text = "Click on your ship to build power. Use that power to fire your weapons. \n Move with WASD";
+
+        score = 0;
+        enemiesAreSpawning = false;
+
+        if (activePlayer == null) SpawnPlayer();
+
+        activeUI.transform.Find("Play Again").gameObject.SetActive(false);
+
+        isRunning = true;
+    }
+
+    public void endGame(bool victory)
+    {
+        isRunning = false;
+        if (score > highScore) highScore = score;
+
+        CancelInvoke("SpawnEnemy");
+
+        string victoryText = victory ? "Congratulations!" : "Game Over!";
+
+        activeUI.transform.Find("Scoreboard").GetComponent<Text>().text = victoryText + "\n Final Score: " + score.ToString() + "\n High Score: " + highScore.ToString();
+        StartCoroutine(DelayedCleanup());
+    }
+
+    IEnumerator DelayedCleanup()
+    {
+        yield return new WaitForSeconds(1);
+
+        activeUI.transform.Find("Play Again").gameObject.SetActive(true);
+    }
+
+    public void AddScore(int amount)
+    {
+        if (!isRunning) return;
+        score += amount;
+        activeUI.transform.Find("Scoreboard").GetComponent<Text>().text = "Score: " + score.ToString();
+    }
+
+    private void SpawnPlayer()
+    {
+        activePlayer = Instantiate(playerShip, new Vector3(-20, 0, 0), Quaternion.identity);
+        activePlayer.GetComponent<ShipController>().activeLevel = this;
+        activePlayer.GetComponent<ShipController>().target = activeUI.transform.Find("Crosshairs");
+    }
+
+    private void SpawnEnemy()
+    {
+        if (!isRunning) return;
+
+        //pick a random spot just off camera
+        float xloc;
+        float yloc;
+        if (Random.value < .5)
+        {//top/bottom
+            //Only spawn on right half 
+            xloc = Random.Range(0, 30);
+            yloc = 17 * (Random.value < .5 ? 1 : -1);
+        } else
+        {//left/right
+            xloc = 29; //Only spawn on right half // * (Random.value < .5 ? 1 : -1);
+            yloc = Random.Range(-16, 16);
+        }
+        Transform newDude = Instantiate(enemy, new Vector3(xloc, yloc), Quaternion.identity);
+        newDude.GetComponent<EnemyController>().activeLevel = this;
+    }
+
+    private void SpawnAsteroid()
+    {
+        if (!isRunning) return;
+
+        Transform newAsteroid = Instantiate(asteroid, new Vector3(30, Random.Range(-18, 18)), Quaternion.identity);
+        newAsteroid.GetComponent<AsteroidController>().activeLevel = this;
+    }
+}
