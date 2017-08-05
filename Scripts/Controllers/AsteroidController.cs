1affe34583d0246874b29a5a2bca2cd3720e40be
Zechw <zechwilliams@gmail.com>
2017-08-05 16:51:54 -0600

    Post LD Bug Fix Update
    - Fix for enemies firing off screen
    - Double laser width for visibility
    - Shields stick to ship
    - Movement limit expanded to full area
    - Destroy leftover enemies on game over, not start (no more free points)

---
+++ Scripts/Controllers/AsteroidController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour {
+
+
+    [HideInInspector] public LevelManager activeLevel;
+
+    public ParticleSystem explosion;
+
+    private float velocity;
+    private float spin;
+    private float scale;
+
+	// Use this for initialization
+	void Start () {
+        velocity = Random.Range(2, 10);
+        spin = Random.Range(-150, 150);
+        scale = Random.Range(.5f, 1.5f);
+        transform.localScale = Vector3.one * scale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * velocity * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.forward * spin * Time.deltaTime);
+	}
+
+    private void OnDestroy()
+    {
+    }
+
+    private void OnBecameInvisible()
+    {
+        //Assumes asteroids only move down
+        if (transform.position.y < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        AudioSource s = Instantiate(activeLevel.boomSound);
+        Destroy(s, 3);
+
+        activeLevel.AddScore(5);
+
+        ParticleSystem expClone = Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(expClone.gameObject, 3);
+
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Scripts/Controllers/StarFieldController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarFieldController : MonoBehaviour {
+
+    public LevelManager activeLevel;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform)
+        {
+           // child.GetComponent<ParticleSystem>().Pause();
+        }
+	}
+	
+	void Update ()
+    {
+        /*
+        //rotate to player
+        if (activeLevel.activePlayer != null)
+        {
+            transform.rotation = activeLevel.activePlayer.transform.rotation;
+        }
+
+        //set speed based on distance to cursor
+        Transform crosshairs = activeLevel.activeUI.transform.Find("Crosshairs");
+        float distance = Vector3.Magnitude(transform.position - crosshairs.position);
+        
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<ParticleSystem>().Simulate(distance / 300, true, false, true);
+        }
+        */
+
+	}
+}
